a61f768f39ed8c9296e69de7147c3bf424bb8d25
Norman Lei <normanlei0901@gmail.com>
2019-11-20 21:00:35 -0800

    buildLead

--- app.js
+++ app.js
@@ -1,48 +1,31 @@
-const Employee = require ("./lib/Employee");
-const Engineer = require ("./lib/Engineer");
-const Intern = require ("./lib/Intern");
-const Manager = require ("./lib/Manager");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 const inquirer = require("inquirer");
+const questions = require("./lib/Questions");
+
+let teamName;
+let objToHTML=[]; //Array to store all the employee object to build html
 
 
 init();
 
-function init(){
-    inquirer.prompt({
-        type: "confirm",
-        message: "Welcome to build your team profolio, are you ready?",
-        name: "start"
-    }).then(function(res){
+function init() {
+    inquirer.prompt(questions.initQuestions).then(function (res) {
         if (res.start) buildTeamLead();
         else init();
     });
 }
 
-function buildTeamLead(){
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "Enter your team's name:",
-            name:"teamName",
-            validate: async (input)=>{
-                if (input.length===0){
-                    console.log("\nPlease enter a team name");
-                    return false;
-                }
-                return true;
-            }
-        },
-        {
-            type: "input",
-            message:"Enter manager's name:",
-            name: "manager",
-            validate: async (input)=>{
-                if (input.length===0 || !input.match(/[a-zA-Z]/i)){
-                    console.log("\nPlease enter a valid name");
-                    return false;
-                }
-                return true;
-            }
-        }
-    ])
+function buildTeamLead() {
+    inquirer.prompt(questions.managerQuestions)
+    .then(function(ans){
+        teamName = ans.teamName;
+        let tempObj = new Manager(data.manager,data.managerID,data.managerEmail,data.managerOffice);
+        objToHTML.push(tempObj);
+        buildStaff();
+    })
+    .catch((err)=>{throw err;});
 }
+
