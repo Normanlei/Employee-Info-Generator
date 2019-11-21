dcbcf996015a02cde611e0181addd854bce35cd8
Norman Lei <normanlei0901@gmail.com>
2019-11-20 22:05:51 -0800

    question finish

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const inquirer = require("inquirer");
 const questions = require("./lib/Questions");
 
 let teamName;
-let objToHTML=[]; //Array to store all the employee object to build html
+let objToHTML = []; //Array to store all the employee object to build html
 
 
 init();
@@ -20,12 +20,58 @@ function init() {
 
 function buildTeamLead() {
     inquirer.prompt(questions.managerQuestions)
-    .then(function(ans){
-        teamName = ans.teamName;
-        let tempObj = new Manager(data.manager,data.managerID,data.managerEmail,data.managerOffice);
-        objToHTML.push(tempObj);
-        buildStaff();
-    })
-    .catch((err)=>{throw err;});
+        .then(function (data) {
+            teamName = data.teamName;
+            let tempObj = new Manager(data.manager, data.managerID, data.managerEmail, data.managerOffice);
+            objToHTML.push(tempObj);
+            buildStaff();
+        }, function (error) {
+            console.log(error);
+        })
+}
+
+function buildStaff() {
+    inquirer.prompt(questions.pickStaffQuestions)
+        .then(function (ans) {
+            switch (ans.pick) {
+                case "Engineer":
+                    buildEngineer();
+                    break;
+                case "Intern":
+                    buildIntern();
+                    break;
+                default:
+                    buildHTML();
+                    break;
+            }
+        },function(error){
+            console.log(error);
+        })
+}
+
+function buildEngineer() {
+    inquirer.prompt(questions.engineerQuestions)
+        .then(function (data) {
+            let tempObj = new Engineer(data.engineer, data.engineerID, data.engineerEmail, data.engineerGithub);
+            objToHTML.push(tempObj);
+            buildStaff();
+        },function(error){
+            console.log(error);
+        })
+}
+
+function buildIntern() {
+    inquirer.prompt(questions.internQuestions)
+        .then(function (data) {
+            let tempObj = new Engineer(data.intern, data.internID, data.internEmail, data.internSchool);
+            objToHTML.push(tempObj);
+            buildStaff();
+        },function(error){
+            console.log(error);
+        })
+}
+
+function buildHTML(){
+    console.log("creating.....html");
 }
 
